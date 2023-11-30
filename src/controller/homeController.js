73e4af8e949ee10024b06498e920885d721dbd86
congit27 <conbo1102a@gmail.com>
2023-11-30 23:03:03 +0700

    fix err2

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,79 +1,74 @@
-import db from "../models/index";
-import homeService from "../service/homeService";
-
-const showLogin = async (req, res) => {
-  return res.render("login/login");
-};
+import db from '../models/index';
+import homeService from '../service/homeService';
 
 const getAllBrandCar = async (req, res) => {
-  try {
-    let data = await homeService.getAllBrandCar();
-    return res.status(200).json({
-      EM: data.EM,
-      EC: data.EC,
-      DT: data.DT,
-    });
-  } catch (e) {
-    console.log(e);
-    return res.status(500).json({
-      EM: "error from server", // error message
-      EC: "-1", // error code
-      DT: "", // data
-    });
-  }
+    try {
+        let data = await homeService.getAllBrandCar();
+        return res.status(200).json({
+            EM: data.EM,
+            EC: data.EC,
+            DT: data.DT,
+        });
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({
+            EM: 'error from server', // error message
+            EC: '-1', // error code
+            DT: '', // data
+        });
+    }
 };
 
 const getAllTypeCar = async (req, res) => {
-  try {
-    let data = await homeService.getAllTypeCar();
-    return res.status(200).json({
-      EM: data.EM,
-      EC: data.EC,
-      DT: data.DT,
-    });
-  } catch (e) {
-    console.log(e);
-    return res.status(500).json({
-      EM: "error from server", // error message
-      EC: "-1", // error code
-      DT: "", // data
-    });
-  }
+    try {
+        let data = await homeService.getAllTypeCar();
+        return res.status(200).json({
+            EM: data.EM,
+            EC: data.EC,
+            DT: data.DT,
+        });
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({
+            EM: 'error from server', // error message
+            EC: '-1', // error code
+            DT: '', // data
+        });
+    }
 };
 
 const getAllColorCar = async (req, res) => {
-  try {
-    let data = await homeService.getAllColorCar();
-    return res.status(200).json({
-      EM: data.EM,
-      EC: data.EC,
-      DT: data.DT,
-    });
-  } catch (e) {
-    console.log(e);
-    return res.status(500).json({
-      EM: "error from server", // error message
-      EC: "-1", // error code
-      DT: "", // data
-    });
-  }
+    try {
+        let data = await homeService.getAllColorCar();
+        return res.status(200).json({
+            EM: data.EM,
+            EC: data.EC,
+            DT: data.DT,
+        });
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({
+            EM: 'error from server', // error message
+            EC: '-1', // error code
+            DT: '', // data
+        });
+    }
 };
 
 const sendHelp = async (req, res) => {
-  console.log("Request Body:", req.body); // Log the request body
-  try {
-    let data = await homeService.handlePostHelp(req.body);
-    return res.status(201).json(data);
-  } catch (e) {
-    console.error(e);
-    return res.status(500).json({ error: "Internal Server Error" });
-  }
+    console.log('Request Body:', req.body); // Log the request body
+    try {
+        let data = await homeService.handlePostHelp(req.body);
+        return res.status(201).json(data);
+    } catch (e) {
+        console.error(e);
+        return res.status(500).json({ error: 'Internal Server Error' });
+    }
 };
 
 module.exports = {
-  showLogin,
-  getAllBrandCar,
-  getAllTypeCar,
-  getAllColorCar,
-  sendHelp,
+    getAllBrandCar,
+    getAllTypeCar,
+    getAllColorCar,
+    sendHelp,
 };
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,33 +1,26 @@
-import express from "express";
-import customerController from "../controller/customerController";
-import partnerControllers from "../controller/partnerControllers";
-import homeController from "../controller/homeController";
+import express from 'express';
+import customerController from '../controller/customerController';
+import partnerControllers from '../controller/partnerControllers';
+import homeController from '../controller/homeController';
 const router = express.Router();
 
 const initApiRoutes = (app) => {
-  // render login page
-  router.get("/", homeController.showLogin);
-  router.get("/api/getAllBrandCar", homeController.getAllBrandCar); //done
-  router.get("/api/getAllTypeCar", homeController.getAllTypeCar); //done
-  router.get("/api/getAllColorCar", homeController.getAllColorCar); //done
+    // render login page
+    router.get('/api/getAllBrandCar', homeController.getAllBrandCar); //done
+    router.get('/api/getAllTypeCar', homeController.getAllTypeCar); //done
+    router.get('/api/getAllColorCar', homeController.getAllColorCar); //done
 
-  app.post("/sendHelp", homeController.sendHelp);
+    app.post('/sendHelp', homeController.sendHelp);
 
-  router.post("/api/login", customerController.handleLoginService);
-  router.post("/api/register", customerController.handleRegisterService);
+    router.post('/api/login', customerController.handleLoginService);
+    router.post('/api/register', customerController.handleRegisterService);
 
-  router.post(
-    "/api/login-partner",
-    partnerControllers.handleLoginPartnerService
-  );
-  router.post(
-    "/api/register-partner",
-    partnerControllers.handleRegisterPartnerService
-  );
+    router.post('/api/login-partner', partnerControllers.handleLoginPartnerService);
+    router.post('/api/register-partner', partnerControllers.handleRegisterPartnerService);
 
-  router.get("/api/get-all-services", partnerControllers.getAllServices);
+    router.get('/api/get-all-services', partnerControllers.getAllServices);
 
-  return app.use("/", router);
+    return app.use('/', router);
 };
 
 export default initApiRoutes;
