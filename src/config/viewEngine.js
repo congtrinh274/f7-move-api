4e9b7b49e3b3b7eacde9e6df5be542994ff82d00
congit27 <conbo1102a@gmail.com>
2023-11-30 22:59:48 +0700

    fix err

--- src/config/viewEngine.js
+++ src/config/viewEngine.js
@@ -1,8 +1,7 @@
-import express from "express";
+import express from 'express';
 
 let configViewEngine = (app) => {
-    app.use(express.static("./src/public"));
-    app.set("view engine", "ejs");
-    app.set("views", "./src/views");
+    app.use(express.static('./src/public'));
+    app.set('view engine', 'ejs');
 };
 export default configViewEngine;
